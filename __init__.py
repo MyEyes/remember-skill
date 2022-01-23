5d871e1b67b2142f703a9be6203e334eeaeaf2ae
Nils Ole Timm <nils.ole.timm@googlemail.com>
2022-01-23 12:46:52 +0100

    Fix incorrect lingua franka import

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
 from mycroft.audio import wait_while_speaking 
 from mycroft.util.log import getLogger
-from lingua_franca import pronounce_number
+from lingua_franca.format import pronounce_number
 from .information_store import LocalFileInformationStore
 
 logger = getLogger(__name__)
